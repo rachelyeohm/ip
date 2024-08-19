b1bcc343cfcdc259e156f4605f9db1c8e0a81a3a
rachelyeohm <rachelyeohm@gmail.com>
2024-08-20 01:02:33 +0800

    Add hello and bye functions, remove mentions of Duke

---
+++ src/main/java/Nyabot.java
@@ -0,0 +1,9 @@
+public class Nyabot {
+    public static void main(String[] args) {
+        String horizontalline = "____________________________";
+        String hellostatement = "Hello! I'm Nyabot. What can I do for you, nya?";
+        String byestatement = "Bye. Hope to see you again, nya!";
+        System.out.println(horizontalline + "\n" + hellostatement + "\n" +
+                horizontalline );
+    }
+}
