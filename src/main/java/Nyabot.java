6dd6dee78ee2ec78a2e88e7f812abda1683bff5a
rachelyeohm <rachelyeohm@gmail.com>
2024-08-20 01:28:59 +0800

    Implement storing and displaying texts in list form

--- src/main/java/Nyabot.java
+++ src/main/java/Nyabot.java
@@ -1,7 +1,10 @@
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Nyabot {
+
+    private static ArrayList<String> textlist = new ArrayList<String>();
     public static void main(String[] args) {
 
         String hellostatement = "Hello! I'm Nyabot. What can I do for you, nya?";
@@ -13,15 +16,34 @@ public class Nyabot {
             String input = scanner.nextLine();
             if (input.equals("bye")) {
                 break;
+            } else if (input.equals("list")) {
+                System.out.println(displaylist());
+                continue;
             }
-            System.out.println(Nyabot.prettifyString(input));
+            Nyabot.textlist.add(input);
+            System.out.println(Nyabot.prettifyString("added: " + input));
         }
         System.out.println(Nyabot.prettifyString(byestatement));
     }
 
     public static String prettifyString(String str) {
-        String horizontalline = "_".repeat(Math.max(40, str.length()));
+        String horizontalline = "_".repeat(70);
         return "\t" + horizontalline + "\n\t" + str + "\n\t" +
                 horizontalline;
+    }
+
+    public static String displaylist() {
+        StringBuilder textliststring = new StringBuilder();
+        for (int i = 0 ; i < Nyabot.textlist.size(); i++) {
+            if (i > 0) {
+                textliststring.append("\t");
+            }
+            textliststring.append((i + 1)).append(". ").append(Nyabot.textlist.get(i));
+            if (i < Nyabot.textlist.size()-1) {
+                textliststring.append("\n");
+            }
+
+        }
+        return prettifyString(textliststring.toString());
     }
 }
