85108889ff4687d737b7c854eb38305ac54f4351
rachelyeohm <rachelyeohm@gmail.com>
2024-08-20 01:11:00 +0800

    Add functionality for echoing input + abstract horizontal lines in string

--- src/main/java/Nyabot.java
+++ src/main/java/Nyabot.java
@@ -1,9 +1,27 @@
+
+import java.util.Scanner;
+
 public class Nyabot {
     public static void main(String[] args) {
-        String horizontalline = "____________________________";
+
         String hellostatement = "Hello! I'm Nyabot. What can I do for you, nya?";
         String byestatement = "Bye. Hope to see you again, nya!";
-        System.out.println(horizontalline + "\n" + hellostatement + "\n" +
-                horizontalline );
+        System.out.println(Nyabot.prettifyString(hellostatement));
+
+        Scanner scanner = new Scanner(System.in);
+        while(true) {
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {
+                break;
+            }
+            System.out.println(Nyabot.prettifyString(input));
+        }
+        System.out.println(Nyabot.prettifyString(byestatement));
+    }
+
+    public static String prettifyString(String str) {
+        String horizontalline = "_".repeat(Math.max(40, str.length()));
+        return "\t" + horizontalline + "\n\t" + str + "\n\t" +
+                horizontalline;
     }
 }
